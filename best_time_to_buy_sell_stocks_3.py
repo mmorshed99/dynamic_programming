5d206d9cc4aa86d469e1e4d00d37dfd32b6d45ef
M Manzur Morshed <mmorshed@asu.edu>
2017-11-25 06:35:25 -0500

    Update best_time_to_buy_sell_stocks_3.py

--- best_time_to_buy_sell_stocks_3.py
+++ best_time_to_buy_sell_stocks_3.py
@@ -1,3 +1,20 @@
+#Say you have an array for which the ith element is the price of a given stock on day i.
+#
+#Design an algorithm to find the maximum profit. You may complete at most two transactions.
+#
+#Note:
+#You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+#
+#Example :
+#
+#Input : [1 2 1 2]
+#Output : 2
+#
+#Explanation : 
+#  Day 1 : Buy 
+#  Day 2 : Sell
+#  Day 3 : Buy
+#  Day 4 : Sell
 class Solution:
     # @param A : tuple of integers
     # @return an integer
